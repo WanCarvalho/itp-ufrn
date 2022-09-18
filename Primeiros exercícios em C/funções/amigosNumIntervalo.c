2a7beedcad671a319d76284fc1fb3c59c457f680
Wanderson Carvalho <wandersoncarvalho.1501@gmail.com>
2022-09-18 19:47:57 -0300

    Resolução dos exercícios amigosNumIntervalo e visorDeRetangulos

---
+++ Primeiros exercícios em C/funções/amigosNumIntervalo.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+//retorna verdadeiro se numero é divisível por divisor e falso em caso contrário
+int testa_divisor(int numero, int divisor){
+    if (numero%divisor == 0){
+        return 1; // 1 é igual a true e 0 igual a false
+    } else {
+        return 0;
+    }
+}
+
+//retorna a soma dos divisores de value no intervalo [1, value)
+int soma_divisores(int value){
+    int somaDivisores = 0;
+
+    for(int i = 1; i < value; i++){
+        if(testa_divisor(value, i) == 1){
+            somaDivisores += i;
+        }
+    }
+
+    return somaDivisores;
+}
+
+//retorna verdadeiro se a é amigo de b e falso em caso contrário
+int testa_amigos(int a, int b){
+    if(soma_divisores(a) == b && soma_divisores(b) == a){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//recebe dois intervalos naturais L1 = [A,B] e L2 = [C,D]
+//apresenta todos os valores em L1 que possuem ao menos um amigo em L2.
+int main(){
+    int L1[2];
+    int L2[2];
+
+    scanf("%d %d %d %d", &L1[0], &L1[1], &L2[0], &L2[1]);
+    
+    for(int i = L1[0]; i <= L1[1]; i++){
+        int contadorDeAmigos;
+        
+        for(int j = L2[0]; j <= L2[1]; j++){
+            if(testa_amigos(i, j) == 1 && i != j){
+               printf("O %d possui um amigo!\n", i); 
+               contadorDeAmigos++;
+            }
+        }
+
+        if(i == L1[1] && contadorDeAmigos == 0){
+            printf("Os intervalos nao apresentam amigos!");
+        }
+         
+    }
+}
---
+++ Primeiros exercícios em C/funções/visorDeRetangulos.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int testa_retangulo(float a, float b, float c, float d, float e, float f, float g, float h){
+
+    if(c > g && d > h && e > a && f > b){
+        printf("(<%.2f,%.2f>,<%.2f,%.2f>) contem (<%.2f,%.2f>,<%.2f,%.2f>).\n", a, b, c, d, e, f, g, h);
+    } else if(g > c && h > d){
+        printf("(<%.2f,%.2f>,<%.2f,%.2f>) contem (<%.2f,%.2f>,<%.2f,%.2f>).\n", e, f, g, h, a, b, c, d);
+    } else {
+        printf("Nao posso afirmar!\n");
+    }
+    return 1;
+}
+
+int main(){
+    float a, b, c, d;
+
+    scanf("%f %f %f %f", &a, &b, &c, &d);
+
+    float l1, l2, l3, l4;
+    int ref = 1;
+
+    do{
+        scanf("%f %f %f %f", &l1, &l2, &l3, &l4);
+
+        //condicional para parar de receber valores quando forem digitados quatro 'zeros'
+        if(l1, l2, l3, l4 == 0){
+            ref = 0;
+            break;
+        }
+
+        testa_retangulo(a, b, c, d, l1, l2, l3, l4);
+
+    } while (ref != 0);
+    
+}
